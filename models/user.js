ab993d44a68dea046be4c018f7cefbc9ea4f3908
chinky23-tech <chinky0330@gmail.com>
2025-10-25 22:39:40 +0530

    user

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    username: { type: String , required: true, unique: true},
-    password: { type: String , required: true}
-})
+const mongoose = require("mongoose");
 
-module.exports = mongoose.model('User', userSchema);
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
